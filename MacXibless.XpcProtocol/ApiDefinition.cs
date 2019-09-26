ea18b38959052e740d583ddca5735953a7ace33b
William Kent <wjk011@gmail.com>
2019-09-25 20:54:46 -0400

    Use delegate type to enable two-way block marshalling

--- MacXibless.XpcProtocol/ApiDefinition.cs
+++ MacXibless.XpcProtocol/ApiDefinition.cs
@@ -7,12 +7,14 @@ using CoreGraphics;
 
 namespace MacXibless
 {
+    delegate void GetHelloStringReturnBlock(NSString message);
+
     [Preserve, XpcInterface]
     [Protocol(Name = "XamarinXpcProtocol")]
     [BaseType(typeof(NSObject))]
     interface XpcProtocol
 	{
         [Export("getHelloString:returnBlock:")]
-		void GetHelloString(NSString toWhom, [BlockCallback] Action<NSString> returnBlock);
+		void GetHelloString(NSString toWhom, [BlockCallback] GetHelloStringReturnBlock returnBlock);
 	}
 }
